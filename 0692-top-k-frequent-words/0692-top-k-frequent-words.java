9b53f372fbb070270dbdf2616fdc27d46332b221
Rhythm Jayee <jayeerythm8@gmail.com>
2022-10-19 09:55:53 +0530

    Time: 17 ms (13.55%), Space: 48.2 MB (5.39%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,24 +1,62 @@
 class Solution {
+    static class TrieNode {
+        String word;
+        TrieNode[] next;
+        TrieNode() {
+            next = new TrieNode[26];
+        }
+    }
     public List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> freq = new HashMap<>();
+        int maxFreq = 0;
         for(String s : words) {
             freq.put(s, freq.getOrDefault(s, 0) + 1);
+            maxFreq = Math.max(maxFreq, freq.get(s));
         }
         
-        PriorityQueue<String> pq = new PriorityQueue<>((a,b) -> {
-            if(freq.get(a) == freq.get(b)) return a.compareTo(b);
-            return freq.get(b) - freq.get(a);
-        });
-        
+        //bucket sort -> Add same freq strings under same Trie
+        TrieNode count[] = new TrieNode[maxFreq + 1];
         for(String s : freq.keySet()) {
-            pq.add(s);
+            int f = freq.get(s);
+            if(count[f] == null) {
+                count[f] = new TrieNode();
+            }
+            addWord(count[f], s);
         }
         
         List<String> ans = new ArrayList<>();
-        while(!pq.isEmpty() && ans.size() < k) {
-            ans.add(pq.remove());
+        //get Words with maxfreq first till size k
+        //as we will be getting characters in lexicographical order
+        //we will get strings in lexicographical order
+        for(int i = maxFreq; i > 0 && ans.size() < k; i--) {
+            if(count[i] != null) {
+                getWord(count[i], k, ans);
+            }
         }
         
         return ans;
+    }
+    private void addWord(TrieNode root, String s) {
+        int n = s.length();
+        for(int i = 0; i<n; i++) {
+            char c = s.charAt(i);
+            if(root.next[c - 'a'] == null) {
+                root.next[c - 'a'] = new TrieNode();
+            }
+            root = root.next[c - 'a'];
+        }
+        root.word = s;
+    } 
+    
+    private void getWord(TrieNode root, int k, List<String> ans) {
+        if(root.word != null) {
+            ans.add(root.word);
+        }
+        
+        for(int i = 0; i<26 && ans.size() < k; i++) {
+            if(root.next[i] != null) {
+                getWord(root.next[i], k, ans);
+            }
+        }
     }
 }
