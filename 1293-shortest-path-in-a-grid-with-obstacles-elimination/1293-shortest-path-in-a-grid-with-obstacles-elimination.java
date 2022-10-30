bee39c612eaa79d24101ba66bb19e89d7b00a244
Rhythm Jayee <jayeerythm8@gmail.com>
2022-10-30 11:16:40 +0530

    Time: 77 ms (37.02%), Space: 54.8 MB (35.60%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -5,35 +5,40 @@ class Solution {
         
         int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1}};
         boolean[][][] visited = new boolean[m][n][k+1];
-        Queue<int[]> q = new LinkedList<>();//i,j,k,steps
-        //i, j, k, steps
-        q.add(new int[]{0,0,0,0});
+        Queue<int[]> q = new LinkedList<>();
+        //i, j, k
+        int steps = 0;
+        q.add(new int[]{0,0,0});
         visited[0][0][0] = true;
         
         while(!q.isEmpty()){
-            int[] rm = q.poll();
-            int x = rm[0];
-            int y = rm[1];
-            int ck = rm[2];
-            int steps = rm[3];
+            int sz = q.size();
+            while(sz-- > 0) {
+                int[] rm = q.poll();
+                int x = rm[0];
+                int y = rm[1];
+                int ck = rm[2];
 
-            if(x == m-1 && y == n-1) return steps;
-            
-            for(int i = 0; i < 4; i++){
-                int nx = x + dirs[i][0];
-                int ny = y + dirs[i][1];
-                int nk = ck;
-            if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-                
-                if(grid[nx][ny] == 1){
-                   nk++;
-                }
-                
-                if(nk <= k && !visited[nx][ny][nk]){
-                    q.add(new int[]{nx,ny,nk,steps+1});
-                    visited[nx][ny][nk] = true;
+                if(x == m-1 && y == n-1) return steps;
+
+                for(int i = 0; i < 4; i++) {
+                    int nx = x + dirs[i][0];
+                    int ny = y + dirs[i][1];
+                    int nk = ck;
+                    
+                    if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+
+                    if(grid[nx][ny] == 1){
+                       nk++;
+                    }
+
+                    if(nk <= k && !visited[nx][ny][nk]){
+                        q.add(new int[]{nx,ny,nk});
+                        visited[nx][ny][nk] = true;
+                    }
                 }
             }
+            if(q.size() != 0) steps++;
         }
         return -1;
     }
