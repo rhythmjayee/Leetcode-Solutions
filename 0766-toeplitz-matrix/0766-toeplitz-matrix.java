b44ab837258046361ab4ca949525a718edd4a7d6
Rhythm Jayee <jayeerythm8@gmail.com>
2022-10-31 09:17:59 +0530

    Time: 1 ms (93.83%), Space: 42.4 MB (90.11%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -2,13 +2,14 @@ class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        Map<Integer, Integer> map = new HashMap<>();
         
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 int val = matrix[i][j];
-                map.putIfAbsent(i - j, val);
-                if(map.get(i - j) != val) return false;
+                //compare with top left (r-1, c-1)
+                //as we go down, we had already checked others ele in top rows
+                //just need to check with top left ele with curr row ele
+                if(i > 0 && j > 0 && matrix[i - 1][j - 1] != val) return false;
             }
         }
         
