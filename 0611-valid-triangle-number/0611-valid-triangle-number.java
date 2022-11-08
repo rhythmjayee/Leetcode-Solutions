0dda1375cf4e9e78e3095383509926c944c44f1f
Rhythm Jayee <jayeerythm8@gmail.com>
2022-11-08 18:54:36 +0530

    Time: 58 ms (35.36%), Space: 43.9 MB (38.92%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -4,15 +4,20 @@ class Solution {
         int count = 0;
         int n = nums.length;
         for(int i = 0; i < n - 2; i++) {
+            //fix 1st side
             int j = i + 1;
             int k = j + 1;
             while(j < n && k < n) {
                 int sumOfSmallerSides = nums[i] + nums[j];
-                if(j != k && sumOfSmallerSides > nums[k]) {
+                //1st, 2nd sides sum > 3rd side
+                if(sumOfSmallerSides > nums[k]) {
+                    //count sides combination bw [j, k] with new k
                     count += k - j;
                     k++;
                 }else {
+                    //2nd side is small, get more larger side
                     j++;
+                    //j should be < k
                     if(j >= k) k++;
                 }
             }
