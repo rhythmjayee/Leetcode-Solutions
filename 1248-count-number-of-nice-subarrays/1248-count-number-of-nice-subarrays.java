e15907791d698545aad22d7fe20d362f409bddf4
Rhythm Jayee <jayeerythm8@gmail.com>
2022-11-10 00:01:39 +0530

    Time: 26 ms (54.31%), Space: 73.1 MB (64.73%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,27 +1,36 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int sum = 0;
+        //atmost(k) - atmost(k-1) = Total(k)
+        return atMostKOddNumsSubarrays(nums, k) - atMostKOddNumsSubarrays(nums, k - 1);
+    }
+    //count subarrays with atmost k odd nos.
+    public int atMostKOddNumsSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int odd = 0;
         int count = 0;
-        for(int x : nums) {
-            //replace all odd with 1 & even with 0
-            //now calc the no. of subarrays with sum = k
-            //use Map + Prefix sum approach
-            if(x % 2 != 0) {
-                sum += 1;
+        int j = 0;
+        for(int i = 0; i<n; i++) {
+            if(nums[i] % 2 != 0) odd++;
+            while(j <= i && odd > k) {
+                if(nums[j++] % 2 != 0) odd--;
             }
-            //count subarray [0, i]
-            if(sum == k) count++;
-            //count subaarays [j, i], j = [1, i - 1]
-            if(map.containsKey(sum - k)) {
-                count += map.get(sum - k);
-            }
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            count += (i - j + 1);
         }
         return count;
     }
 }
 /*
- 2,2,2,1,2,2,1,2,2,2
- 0 0 0 1 1 1 2 2 2 2
+1,1,2,1,1
+atmost k=3
+1
+11 1
+112 12 2
+1121 121 21 1
+1211 211 11 1
+atmost k=2
+1
+11 1
+112 12 2
+121 21 1
+211 11 1
 */
