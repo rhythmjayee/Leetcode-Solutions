bf147d5f00e555e07b06fd7cb70c7d0b90c4f121
Rhythm Jayee <jayeerythm8@gmail.com>
2022-11-09 18:09:28 +0530

    Time: 0 ms (100.00%), Space: 43 MB (8.60%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -6,17 +6,11 @@ class Solution {
         
         while(i <= j && j >= 0) {
             if(nums[i] == val) {
-                swap(nums, i, j);
-                j--;
+                nums[i] = nums[j--];
             }else {
                 i++;
             }
         }
         return i;
-    }
-    private void swap(int[] nums, int i, int j) {
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
     }
 }
