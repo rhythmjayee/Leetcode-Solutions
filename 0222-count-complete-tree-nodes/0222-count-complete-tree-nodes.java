841fd2800d95555d41f05dae6e089b11cffd4211
Rhythm Jayee <jayeerythm8@gmail.com>
2022-11-15 09:28:11 +0530

    Time: 1 ms (27.42%), Space: 49.9 MB (41.59%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,36 +14,41 @@
  * }
  */
 class Solution {
+    private Map<TreeNode, Integer> map = new HashMap<>();
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
 
-        int left = getHeightLeft(root);
-        int right = getHeightRight(root);
+        int left = getHeightLeft(root.left);
+        int right = getHeightRight(root.right);
 
         //If left and right are equal it means that the tree is complete and hence go for 2^h -1.
-        if (left == right) return ((2 << (left)) - 1);
+        if (left == right) return ((2 << (left + 1)) - 1);
 
         //else recursively calculate the number of nodes in left and right and add 1 for root.
-        return countNodes(root.left) + countNodes(root.right) + 1;
+        int nodes = countNodes(root.left) + countNodes(root.right) + 1;
+        // System.out.println(root.val+" nodes-> "+nodes);
+        return nodes;
     }
 
 
     public int getHeightLeft(TreeNode root) { //logh
-        int count = 0;
-        while (root.left != null) {
-            count++;
-            root = root.left;
-        }
-        return count;
+        if(root == null) return -1;
+        else if(map.containsKey(root)) return map.get(root);
+         
+        int l = getHeightLeft(root.left) + 1;
+        map.put(root, l);
+        // System.out.println(root.val+" -> "+l);
+        return l;
     }
 
 
     public int getHeightRight(TreeNode root) {
-        int count = 0;
-        while (root.right != null) {
-            count++;
-            root = root.right;
-        }
-        return count;
+        if(root == null) return -1;
+        else if(map.containsKey(root)) return map.get(root);
+         
+        int r = getHeightRight(root.right) + 1;
+        map.put(root, r);
+         // System.out.println(root.val+" -> "+r);
+        return r;
     }
 }
