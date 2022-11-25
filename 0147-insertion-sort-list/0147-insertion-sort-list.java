0edb62c200179ddb57446640169533e016c1d2fb
Rhythm Jayee <jayeerythm8@gmail.com>
2022-11-25 10:24:04 +0530

    Time: 27 ms (31.44%), Space: 44.4 MB (66.80%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -10,36 +10,23 @@
  */
 class Solution {
     public ListNode insertionSortList(ListNode head) {
-        if(head == null || head.next == null) return head;
-        ListNode dumy = new ListNode();
-        //node after that we need to replace the smaller node
-        ListNode prev = dumy;
-        //node before that we need to replace the smaller node
-        ListNode tmp = head;
-        //node to be replaced
-        ListNode node = head.next;
-        ListNode nodePrev = head;
-        
-        dumy.next = head;
-        //prev tmp nodePrev node nxt
-        while(node != null) {
-            ListNode nxt = node.next;
-            while(tmp.val < node.val) {
-                prev = tmp;
-                tmp = tmp.next;
-            }
-            //Join node in its correct position
-            prev.next = node;
-            //handle case if node already in its correct position
-            node.next = tmp == node ? nxt : tmp;
-            nodePrev.next = tmp == node ? node : nxt;
-            nodePrev = tmp == node ? node : nodePrev;
-            //move to next node to find its correct position
-            node = nxt;
-            //reset searching nodes
-            tmp = dumy.next;
-            prev = dumy;
-        }
-        return dumy.next;
+      ListNode curr = head, next = null;
+      // l is a fake head
+      ListNode l = new ListNode(0);
+
+      while (curr != null) {
+        next = curr.next;
+
+        ListNode p = l;
+        while (p.next != null && p.next.val < curr.val)
+          p = p.next;
+
+        // insert curr between p and p.next
+        curr.next = p.next;
+        p.next = curr;
+        curr = next;
+      }
+
+      return l.next;
     }
 }
