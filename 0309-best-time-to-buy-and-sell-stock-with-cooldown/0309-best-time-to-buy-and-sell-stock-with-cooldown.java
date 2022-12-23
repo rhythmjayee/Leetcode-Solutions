b1d937e771c1e3ffe266029d3a4aff9ad2de30b3
Rhythm Jayee <jayeerythm8@gmail.com>
2022-12-23 10:58:59 +0530

    Time: 3 ms (30.66%), Space: 42.4 MB (42.93%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,25 +1,25 @@
 class Solution {
-    public int maxProfit(int[] p) {
-        return td(p, 1, 0, new Integer[p.length][2]);
-    }
-    private int td(int[] p, int buy, int i, Integer[][] dp) {
-        if(i >= p.length) return 0;
-        else if(dp[i][buy] != null) return dp[i][buy];
-        
-        int b = Integer.MIN_VALUE;
-        int s = Integer.MIN_VALUE;
-        int nb = Integer.MIN_VALUE;
-        int ns = Integer.MIN_VALUE;
-        
-        if(buy == 1) {
-            b = -p[i] + td(p, 0, i + 1, dp);
-            nb = td(p, 1, i + 1, dp);
-        }else if(buy == 0) {
-            s = p[i] + td(p, 1, i + 2, dp);
-            ns = td(p, 0, i + 1, dp);
+    public int maxProfit(int[] prices) {
+        /**
+        [],i,canBuy
+        if(i == n) return 0;
+        if(canBuy == 1){
+            buy = -[i] + ([],i+1,0);
+            notBuy = ([],i+1,1);
+            return max
+        }else if( prevSell == 0){
+            sell = [i]+([],i+2,1);
+            notSell = ([],i+1,0);
+            return max;
         }
+        */
+        int n = prices.length;
+        int[][] dp = new int[n+2][2];
         
-        dp[i][buy] = Math.max(Math.max(b, nb), Math.max(s, ns));
-        return dp[i][buy];
+        for(int i = n-1; i>=0 ;i--){
+            dp[i][1] = Math.max(-prices[i] + dp[i+1][0],dp[i+1][1]);
+            dp[i][0] = Math.max(prices[i] + dp[i+2][1],dp[i+1][0]);
+        }
+        return dp[0][1];
     }
 }
