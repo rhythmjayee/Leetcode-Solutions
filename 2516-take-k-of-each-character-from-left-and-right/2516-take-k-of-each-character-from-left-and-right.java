d6032ad3ab5c49798fb552df01798fb065a9e64b
Rhythm Jayee <jayeerythm8@gmail.com>
2022-12-25 11:58:25 +0530

    Time: 11 ms (100.00%), Space: 43.7 MB (75.00%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.java
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.java
@@ -3,6 +3,7 @@ class Solution {
         int ans = Integer.MAX_VALUE;
         int s = 0;
         int[] f = new int[3];
+        //get count of all the chars
         for(char c : st.toCharArray()) {
             f[c - 'a']++;
         }
@@ -10,11 +11,16 @@ class Solution {
         int n = st.length();
         for(int e = 0; e<n; e++) {
             char c = st.charAt(e);
+            //make a discarded window -> other than remaing window which is the ans
             f[c - 'a']--;
+            //f[0] >= k && f[1] >= k && f[2] >= k
+            //if any of the conditions fails -> 
+            //decerease the discarded window from front by s++
             while(s <= e && (f[0] < k || f[1] < k || f[2] < k)) {
                 char cc = st.charAt(s++);
                 f[cc - 'a']++;
             }
+            //Condition satifies -> remaing window may be the ans
             ans = Math.min(ans, n - (e - s + 1));
         }
         return ans;
