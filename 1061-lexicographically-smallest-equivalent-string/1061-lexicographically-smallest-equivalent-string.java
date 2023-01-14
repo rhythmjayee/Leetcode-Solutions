18ec51b79f6e79702a3063d40edfdaf2af75808b
Rhythm Jayee <jayeerythm8@gmail.com>
2023-01-14 18:59:16 +0530

    Time: 1 ms (100.00%), Space: 40 MB (100.00%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
@@ -1,60 +1,40 @@
 class Solution {
-    int minChar;
-
-    void DFS(int src, Integer[][] adjMatrix, Integer visited[], List<Integer> component) {
-        // Mark the character as visited.
-        visited[src] = 1;
-        // Add it to the list.
-        component.add(src);
-        // Update the minimum character in the component.
-        minChar = Math.min(minChar, src);
-
-        for (int i = 0; i < 26; i++) {
-            // Perform DFS if the edge exists and the node isn't visited yet.
-            if (adjMatrix[src][i] != null && visited[i] == null) {
-                DFS(i, adjMatrix, visited, component);
-            }
-        }
+    int[] parent;
+    //s-e a-o e-o
+    private int find(int ch) {
+        if(ch == parent[ch]) return ch;
+        return parent[ch] = find(parent[ch]);
     }
-
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
-        // Adjacency matrix to store edges.
-        Integer adjMatrix[][] = new Integer[26][26];
-        for (int i = 0; i < s1.length(); i++) {
-            adjMatrix[s1.charAt(i) - 'a'][s2.charAt(i) - 'a'] = 1;
-            adjMatrix[s2.charAt(i) - 'a'][s1.charAt(i) - 'a'] = 1;
+        parent = new int[26];
+        int n = s1.length();
+        int m = baseStr.length();
+        
+        for(int i = 0; i<26; i++) {
+            parent[i] = i;
         }
-
-        // Array to store the final character mappings.
-        int mappingChar[] = new int[26];
-        for (int i = 0; i < 26; i++) {
-            mappingChar[i] = i;
-        }
-
-        // Array to keep visited nodes during DFS.
-        Integer visited[] = new Integer[26];
-        for (int c = 0; c < 26; c++) {
-            if (visited[c] == null) {
-                // Store the characters in the current component.
-                List<Integer> component = new ArrayList<>();
-                // Variable to store the minimum character in the component.
-                minChar = 27;
-
-                DFS(c, adjMatrix, visited, component);
-
-                // Map the characters in the component to the minimum character.
-                for (int vertex : component) {
-                    mappingChar[vertex] = minChar;
-                }
+        
+        for(int i = 0 ; i<n; i++) {
+            int c1 = s1.charAt(i) - 'a';
+            int c2 = s2.charAt(i) - 'a';
+            //get parent of both chrs
+            int p1 = find(c1);
+            int p2 = find(c2);
+            
+            if(p1 == p2) continue;
+            else if(p1 < p2) {//allocate parent which is smaller
+                parent[p2] = p1;
+            }else {
+                parent[p1] = p2;
             }
         }
-
-        String ans = "";
-        // Create the answer string.
-        for (char c : baseStr.toCharArray()) {
-            ans += (char)(mappingChar[c - 'a'] + 'a');
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<m; i++) {
+            int c = baseStr.charAt(i) - 'a';
+            char ch = (char)(find(c) + 'a');
+            sb.append(ch);
         }
-
-        return ans;
+        return sb.toString();
     }
 }
