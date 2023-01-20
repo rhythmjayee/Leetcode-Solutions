020cb224733dc0f850fcbb176174eeba759f6d5f
Rhythm Jayee <jayeerythm8@gmail.com>
2023-01-20 10:56:27 +0530

    Time: 2336 ms (5.27%), Space: 49.5 MB (61.82%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -1,31 +1,24 @@
 class Solution {
-    private void backtrack(int[] nums, int index, List<Integer> sequence,
-            Set<List<Integer>> result) {
-        // if we have checked all elements
-        if (index == nums.length) {
-            if (sequence.size() >= 2) {
-                result.add(new ArrayList<>(sequence));
-            }
-            return;
-        }
-        // if the sequence remains increasing after appending nums[index]
-        if (sequence.isEmpty() ||
-                sequence.get(sequence.size() - 1) <= nums[index]) {
-            // append nums[index] to the sequence
-            sequence.add(nums[index]);
-            // call recursively
-            backtrack(nums, index + 1, sequence, result);
-            // delete nums[index] from the end of the sequence
-            sequence.remove(sequence.size() - 1);
-        }
-        // call recursively not appending an element
-        backtrack(nums, index + 1, sequence, result);
-    }
-
     public List<List<Integer>> findSubsequences(int[] nums) {
-        Set<List<Integer>> result = new HashSet<List<Integer>>();
-        List<Integer> sequence = new ArrayList<Integer>();
-        backtrack(nums, 0, sequence, result);
-        return new ArrayList(result);
+        Set<List<Integer>> ans = new HashSet<>();
+        helper(nums, 0, -1, ans, new ArrayList<>());
+        return new ArrayList<>(ans);
+    }
+    private void helper(int[] nums, int newStartIndex, int prevIndex,  Set<List<Integer>> ans, List<Integer> ls) {
+        if(ls.size() >= 2 && newStartIndex == nums.length) {
+            ans.add(new ArrayList<>(ls));
+            return;
+        }else if(newStartIndex == nums.length) return;
+        
+        for(int k = newStartIndex; k<nums.length; k++) {
+            if(prevIndex == -1 || nums[prevIndex] <= nums[k]) {                
+                ls.add(nums[k]);
+                helper(nums, k + 1, k, ans, ls);
+                ls.remove(ls.size() - 1);
+            }
+            
+            helper(nums, k + 1, prevIndex, ans, ls);
+        }
+        
     }
 }
