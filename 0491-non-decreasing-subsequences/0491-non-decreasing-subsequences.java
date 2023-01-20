5716d7f9cb5b00dd41b284c96e384243b45aa825
Rhythm Jayee <jayeerythm8@gmail.com>
2023-01-20 11:08:32 +0530

    Time: 6 ms (87.64%), Space: 48.1 MB (88.00%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -1,24 +1,25 @@
 class Solution {
     public List<List<Integer>> findSubsequences(int[] nums) {
-        Set<List<Integer>> ans = new HashSet<>();
-        helper(nums, 0, -1, ans, new ArrayList<>());
-        return new ArrayList<>(ans);
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(nums, 0, ans, new ArrayList<>());
+        return ans;
     }
-    private void helper(int[] nums, int newStartIndex, int prevIndex,  Set<List<Integer>> ans, List<Integer> ls) {
-        if(ls.size() >= 2 && newStartIndex == nums.length) {
+    private void helper(int[] nums, int newStartIndex,  List<List<Integer>> ans, List<Integer> ls) {
+        //add all the seq if size > 1
+        if(ls.size() > 1) {
             ans.add(new ArrayList<>(ls));
-            return;
-        }else if(newStartIndex == nums.length) return;
-        
-        for(int k = newStartIndex; k<nums.length; k++) {
-            if(prevIndex == -1 || nums[prevIndex] <= nums[k]) {                
-                ls.add(nums[k]);
-                helper(nums, k + 1, k, ans, ls);
-                ls.remove(ls.size() - 1);
-            }
-            
-            helper(nums, k + 1, prevIndex, ans, ls);
         }
         
+        //To avoid using same numbers at same recursion call
+        Set<Integer> visitedNow = new HashSet<>();
+        for(int k = newStartIndex; k<nums.length; k++) {
+            if(visitedNow.contains(nums[k])) continue;
+            else if(ls.size() == 0 || ls.get(ls.size() - 1) <= nums[k]) {   
+                visitedNow.add(nums[k]);
+                ls.add(nums[k]);
+                helper(nums, k + 1, ans, ls);
+                ls.remove(ls.size() - 1);
+            }
+        }
     }
 }
