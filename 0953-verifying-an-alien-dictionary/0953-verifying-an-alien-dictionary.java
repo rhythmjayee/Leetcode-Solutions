465d93d6898f67e6e567eb579188885157dfb524
Rhythm Jayee <jayeerythm8@gmail.com>
2023-02-02 09:47:33 +0530

    Time: 1 ms (65.29%), Space: 42.3 MB (38.76%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -7,20 +7,19 @@ class Solution {
         
         int n = words.length;
         for(int i = 0; i < n - 1; i++) {
-            for(int j = i + 1; j < n; j++) {
-                int min = Math.min(words[i].length() , words[j].length());
-                int f = 0;
-                for(int k = 0; k < min; k++) {
-                    if(words[i].charAt(k) != words[j].charAt(k)) {
-                        if(map.get(words[i].charAt(k)) < map.get(words[j].charAt(k))) {
-                            f = 1;
-                            break;
-                        }
-                        else return false;
+            int j = i + 1;
+            int min = Math.min(words[i].length() , words[j].length());
+            int f = 0;
+            for(int k = 0; k < min; k++) {
+                if(words[i].charAt(k) != words[j].charAt(k)) {
+                    if(map.get(words[i].charAt(k)) < map.get(words[j].charAt(k))) {
+                        f = 1;
+                        break;
                     }
+                    else return false;
                 }
-                if(f == 0 && words[i].length() > words[j].length()) return false;
             }
+            if(f == 0 && words[i].length() > words[j].length()) return false;
         }
         return true;
     }
