8e01b414f70637784d30e26ca64a2da46041c0cb
Rhythm Jayee <jayeerythm8@gmail.com>
2023-02-20 07:44:26 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (93.24%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,13 +1,18 @@
 class Solution {
     public int searchInsert(int[] nums, int t) {
         int n = nums.length;
+        //set start, end points
         int i = 0;
         int j = n;
         
         while(i < j) {
+            //get middle index
             int md = i + ((j - i) >> 1);
             if(nums[md] == t) return md;
+            //if nums[md] > t -> set end point as md -> md can be the index
             else if(nums[md] > t) j = md;
+            //if nums[md] < t -> set start point as md + 1 
+            //-> md cant be the index as it occupied by smaller element nums[md]
             else i = md + 1;
         }
         return j;
