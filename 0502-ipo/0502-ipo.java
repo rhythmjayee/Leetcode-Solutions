4a5ba8d2d7fec0abe33a45c6b9defcdca00cd66e
Rhythm Jayee <jayeerythm8@gmail.com>
2023-02-23 07:29:15 +0530

    Time: 135 ms (25.43%), Space: 111.2 MB (5.12%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,39 +1,32 @@
 class Solution {
-    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        int n = profits.length;
-        int[][] project = new int[n][2];
-        for(int i = 0; i<n; i++) {
-            project[i][0] = profits[i];
-            project[i][1] = capital[i];
+    public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
+      //MAx heap for profit 
+       PriorityQueue<int[]> maxheap=new PriorityQueue<>((int[]a,int[]b)->{
+         return b[1]-a[1];
+      });
+      //MIN heap for captial
+      PriorityQueue<int[]> minheap = new PriorityQueue<>((int[]a,int[]b)->{
+          return a[0]-b[0];
+      });
+      
+      for(int i = 0; i < Profits.length; i++) {
+          minheap.add(new int[]{Capital[i],Profits[i]});
+      }
+
+      int total = W;
+      
+      while(k-- >0){
+         //add all projects in max heap for profit whose capital <= total
+         while(!minheap.isEmpty() && minheap.peek()[0]<=total){
+          maxheap.add(minheap.poll());
         }
-        //sort on starting capital of project
-        Arrays.sort(project, (p1, p2) -> p1[1] - p2[1]);
-        //pq of max net profit of project 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((p1, p2) -> {
-            return p2 - p1;
-        });
-        
-        int gain = w;
-        int i = 0;
-        while(i < n && k > 0) {
-            //add project to pq whose capital <= gain
-            while(i < n && gain >= project[i][1]) {
-                pq.add(project[i++][0]);
-            }
-            if(pq.isEmpty()) break;
-            //get gains from projects untill 
-            //we can add more project with higher capital
-            while(!pq.isEmpty() && k > 0) {
-                gain += pq.remove();
-                k--;
-                if(i < n && gain >= project[i][1]) break;
-            }
-        }
-        //if k > 0
-        while(!pq.isEmpty() && k > 0) {
-            gain += pq.remove();
-            k--;
-        }
-        return gain;
+        //no valid project break
+        if(maxheap.isEmpty()) break;
+
+        int p[] = maxheap.poll();
+        total += p[1];
+       
+      }
+      return total;
     }
 }
