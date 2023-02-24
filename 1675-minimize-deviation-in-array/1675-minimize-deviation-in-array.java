fbe42b67f92e83ed0ca584b2ff69a845be43e6c7
Rhythm Jayee <jayeerythm8@gmail.com>
2023-02-24 07:23:18 +0530

    Time: 666 ms (26.41%), Space: 51.2 MB (33.96%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
@@ -1,22 +1,33 @@
 class Solution {
-    public int minimumDeviation(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        int min = Integer.MAX_VALUE;
-        int res = Integer.MAX_VALUE;
-        for (int a : nums)  {
-            if (a % 2 == 1) {
-                a = a * 2;
+   public int minimumDeviation(int[] nums) {
+        PriorityQueue<Pair<Integer, Integer>> minHeap = new PriorityQueue<>((o1, o2) -> o1.getKey() - o2.getKey());
+        int maxOddFromEven = 0;//on dividing /2 , max odd num we can get from all even nums
+        int result = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            int oddFromEven = num;
+            while (oddFromEven % 2 == 0)
+                oddFromEven /= 2;
+
+            maxOddFromEven = Math.max(maxOddFromEven, oddFromEven);
+            minHeap.add(new Pair<Integer, Integer>(oddFromEven, num));
+        }
+    
+        //initally heap will have pairs like {odd, odd} & {oddFromEven, even}
+        while (minHeap.size() == nums.length) {
+            var pair = minHeap.poll();
+            result = Math.min(result, maxOddFromEven - pair.getKey());
+
+            //we want to hit a condition which dont follows below one
+            //so that loop breaks
+            if (pair.getKey() % 2 == 1 || pair.getKey() < pair.getValue()) {
+                //this condition will try to make a pairs like {evenFormOdd, odd} & {smallerEven, even}
+                minHeap.add(new Pair<Integer, Integer>(pair.getKey() * 2, pair.getValue()));
+                //update maxOddFromEven
+                maxOddFromEven = Math.max(maxOddFromEven, pair.getKey() * 2);
             }
-            pq.offer(a);
-            min = Math.min(min, a);
         }
-        while (pq.peek() % 2 == 0) {
-            res = Math.min(res, pq.peek() - min);
-            min = Math.min(min, pq.peek() / 2);
-            pq.offer(pq.peek() / 2);
-            pq.poll();
-        }
-        return Math.min(res, pq.peek() - min);
-        
+
+        return result;
     }
 }
