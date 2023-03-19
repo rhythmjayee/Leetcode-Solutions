f3f58d68a9473de9108d972de8beab41dcd17fc5
Rhythm Jayee <jayeerythm8@gmail.com>
2023-03-19 08:15:05 +0530

    Time: 375 ms (97.24%), Space: 102.4 MB (37.93%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,50 @@
+class TrieNode {
+    public boolean isWord; 
+    public TrieNode[] children = new TrieNode[26];
+}
+class WordDictionary {
+  private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode(); 
+    }
+    
+    public void addWord(String word) {
+        TrieNode ws = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(ws.children[c - 'a'] == null){
+                ws.children[c - 'a'] = new TrieNode();
+            }
+            ws = ws.children[c - 'a'];
+        }
+        ws.isWord = true;
+    }
+
+    public boolean search(String word) {
+        return match(word.toCharArray(), 0, root);
+    }
+    
+    private boolean match(char[] chs, int k, TrieNode node) {
+        if (k == chs.length) return node.isWord;   
+        if (chs[k] != '.') {
+            return node.children[chs[k] - 'a'] != null && match(chs, k + 1, node.children[chs[k] - 'a']);
+        } else {
+            for (int i = 0; i < node.children.length; i++) {
+                if (node.children[i] != null) {
+                    if (match(chs, k + 1, node.children[i])) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
