7b20e595a68a75e2c67eb0c59bc3c5e1a60a4393
Rhythm Jayee <jayeerythm8@gmail.com>
2023-04-05 08:04:28 +0530

    Time: 9 ms (74.55%), Space: 62.2 MB (30.11%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -6,7 +6,7 @@ class Solution {
         // Iterate over nums, update prefix sum and answer.
         for (int i = 0; i < nums.length; ++i) {
             prefixSum += nums[i];
-            answer = Math.max(answer, (prefixSum + i) / (i + 1));
+            answer = Math.max(answer, (long)Math.ceil((double)(prefixSum) / (i + 1)));
         }
 
         return (int)answer;
