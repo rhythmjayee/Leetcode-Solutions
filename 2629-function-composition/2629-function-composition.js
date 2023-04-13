e60151af27464feb09e1ba5f1b81d73ee8db61d9
Rhythm Jayee <jayeerythm8@gmail.com>
2023-04-13 17:45:09 +0530

    Time: 82 ms (100.00%), Space: 43.5 MB (100.00%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    if(functions.length === 0) return function(x) {
+        return x
+    }
+    for(let i = functions.length - 2; i>=0; i--) {
+        let f_1 = functions[i + 1]
+        let f = functions[i]
+        let nf = function(x) {
+            x = f_1(x)
+            return f(x)
+        }
+        functions[i] = nf
+    }
+    return functions[0];
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
