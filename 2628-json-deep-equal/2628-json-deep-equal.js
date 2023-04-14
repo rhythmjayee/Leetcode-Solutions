55434cdbae64bbefaa79d57eea31610935467d9e
Rhythm Jayee <jayeerythm8@gmail.com>
2023-04-14 20:28:15 +0530

    Time: 69 ms (72.41%), Space: 45.7 MB (93.10%) - LeetHub

---
+++ 2628-json-deep-equal/2628-json-deep-equal.js
@@ -0,0 +1,27 @@
+/**
+ * @param {any} o1
+ * @param {any} o2
+ * @return {boolean}
+ */
+var areDeeplyEqual = function(o1, o2) {
+    if (o1 === o2) {
+        return true;
+    }
+    if (typeof o1 !== 'object' || o1 === null || typeof o2 !== 'object' || o2 === null) {
+        return false;
+    }
+    if (Array.isArray(o1) !== Array.isArray(o2)) {
+        return false;
+    }
+    const keys1 = Object.keys(o1);
+    const keys2 = Object.keys(o2);
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+    for(let key of keys1){
+        if(keys2.includes(key) === false) return false
+        let checkNextLvl = areDeeplyEqual(o1[key], o2[key])
+        if(checkNextLvl === false) return false
+    }
+    return true
+};
