80e2ec64fa9df96cf7f149bba482f7d4b0337b3e
Rhythm Jayee <jayeerythm8@gmail.com>
2023-04-15 15:49:20 +0530

    Time: 112 ms (43.72%), Space: 53.3 MB (17.04%) - LeetHub

--- 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -4,8 +4,18 @@
  * @return {boolean}
  */
 var checkIfInstanceOf = function(obj, classFunction) {
-    if(obj === null || obj === undefined || typeof classFunction !== 'function') return false;
-    return Object(obj) instanceof classFunction;
+    while(obj!=null)
+    {
+        if(obj.constructor === classFunction)
+        {
+            return true;
+        }
+
+        obj = Object.getPrototypeOf(obj);
+
+    }
+
+    return false;
 };
 
 /**
