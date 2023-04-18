2161df08baa028f240fe70c41bb4108df4d4ac4a
Rhythm Jayee <jayeerythm8@gmail.com>
2023-04-18 08:01:37 +0530

    Time: 2 ms (42.98%), Space: 42.4 MB (38.53%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i = 0;
+        int j = 0;
+        int n = word1.length();
+        int m = word2.length();
+        StringBuilder sb = new StringBuilder();
+        
+        while(i < n || j < m) {
+            if(i < n) {
+                char a = word1.charAt(i++);
+                 sb.append(a+"");
+            }
+            if(j < m) {
+                char b = word2.charAt(j++);
+                sb.append(b+"");
+            }
+        }
+        return sb.toString();
+    }
+}
