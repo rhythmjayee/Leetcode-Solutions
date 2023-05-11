0657231789ccf8031e6ea794c0490ebad90dd8af
Rhythm Jayee <jayeerythm8@gmail.com>
2023-05-11 08:37:56 +0530

    Time: 65 ms (88.27%), Space: 43.2 MB (63.29%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -1,22 +1,14 @@
-/**
- * @param {Function[]} functions
- * @return {Function}
- */
-var compose = function(functions) {
-    if(functions.length === 0) return (x) => x;
-    for(let i = functions.length - 2; i>=0; i--) {
-        let f_1 = functions[i + 1]
-        let f = functions[i]
-        let nf = (x) => {
-            x = f_1(x)
-            return f(x)
-        }
-        functions[i] = nf
-    }
-    return functions[0];
-};
+var compose = function (functions) {
+  return function (x) {
+    if (functions.length === 0) return x;
+    let input = x;
 
-/**
- * const fn = compose([x => x + 1, x => 2 * x])
- * fn(4) // 9
- */
+    for (let i = functions.length - 1; i >= 0; i--) {
+      const currFunc = functions[i];
+
+      input = currFunc(input);
+    }
+
+    return input;
+  };
+};
