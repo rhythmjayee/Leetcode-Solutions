b79b84f2491006d73292b4db656105184d211df5
Rhythm Jayee <jayeerythm8@gmail.com>
2023-05-22 10:16:59 +0530

    Time: 72 ms (67.33%), Space: 47.9 MB (65.29%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
@@ -5,19 +5,20 @@
 var jsonStringify = function(obj) {
     if(obj === null) return null
     else if(obj === true || obj === false) return obj === true
-    else if(typeof obj === 'string') return "\"" + obj + "\""
+    else if(typeof obj === 'string') return "\"" + obj + "\"" //string -> "string"
     else if(Number.isInteger(obj)) return Number(obj)
     
     let keys = Object.keys(obj)
     let isArray = Array.isArray(obj)
     let ans = isArray ? "[" : "{"
     for(let key of keys) {
-        let res = jsonStringify(obj[key])
+        let res = jsonStringify(obj[key])//convert the val
         if(isArray) {
             if(ans[ans.length - 1] !== "[") ans += ","
             ans += res
         }else {
             if(ans[ans.length - 1] !== "{") ans += ","
+            // "key":stringify_value
             ans += "\"" + key + "\"" + ":" + res
         }
     }
