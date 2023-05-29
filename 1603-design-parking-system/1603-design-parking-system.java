a47c4ad12b31903c87608b6fa555408072200ee6
Rhythm Jayee <jayeerythm8@gmail.com>
2023-05-29 09:50:48 +0530

    Time: 9 ms (56.91%), Space: 44.9 MB (6.49%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.java
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -1,19 +1,18 @@
 class ParkingSystem {
     private static final int CAR_TYPES = 3;
-    private Map<Integer, Integer> parking;
+    private int[] parking;
     private int[] limits;
     
     public ParkingSystem(int big, int medium, int small) {
-        parking = new HashMap<>();
-        limits = new int[CAR_TYPES + 1];
-        limits[1] = big;
-        limits[2] = medium;
-        limits[3] = small;
+        parking = new int[CAR_TYPES + 1];
+        parking[1] = big;
+        parking[2] = medium;
+        parking[3] = small;
     }
     
     public boolean addCar(int carType) {
-        if(parking.getOrDefault(carType, 0) == limits[carType]) return false;
-        parking.put(carType, parking.getOrDefault(carType, 0) + 1);
+        if(parking[carType] == 0) return false;
+        parking[carType]--;
         return true;
     }
 }
