a020ba636a10c81db1406119506d0deb4c09a3ba
Rhythm Jayee <jayeerythm8@gmail.com>
2023-06-01 22:12:39 +0530

    Time: 246 ms (31.55%), Space: 93 MB (7.34%) - LeetHub

--- 2649-nested-array-generator/2649-nested-array-generator.js
+++ 2649-nested-array-generator/2649-nested-array-generator.js
@@ -1,28 +1,9 @@
-/**
- * @param {Array} arr
- * @return {Generator}
- */
 var inorderTraversal = function*(arr) {
-    let ans = depp(arr)
-    for(let a of ans) yield a
-};
-function depp(arr) {
-    let ans = []
-    for(let a of arr) {
-        if(Array.isArray(a)) {
-            let rs = depp(a)
-            ans = ans.concat(rs)
-        } else {
-           ans.push(a)
+    if (Array.isArray(arr)) {
+        for (let a of arr) {
+            yield* inorderTraversal(a);
         }
-        
+    } else {
+        yield arr;
     }
-    return ans
-}
-
-/**
- * const gen = inorderTraversal([1, [2, 3]]);
- * gen.next().value; // 1
- * gen.next().value; // 2
- * gen.next().value; // 3
- */
+};
