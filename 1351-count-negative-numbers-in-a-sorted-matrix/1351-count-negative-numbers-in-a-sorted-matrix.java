0ce01f7823fb53a51e96b33e226ad7c45e8382d7
Rhythm Jayee <jayeerythm8@gmail.com>
2023-06-08 08:13:33 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (11.61%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -1,18 +1,23 @@
 class Solution {
-    public int countNegatives(int[][] g) {
+    public int countNegatives(int[][] grid) {
         int count = 0;
-        int m = g[0].length;
-        int currRowNegativeIndex = m - 1;
-
+        int n = grid[0].length;
         // Iterate on all rows of the matrix one by one.
-        for (int[] row : g) {
-            // Decrease 'currRowNegativeIndex' so that it points to current row's last positive element.
-            while (currRowNegativeIndex >= 0 && row[currRowNegativeIndex] < 0) {
-                currRowNegativeIndex--;
+        for (int[] row : grid) {
+            // Using binary search find the index
+            // which has the first negative element.
+            int left = 0, right = n;
+            while (left < right) {
+                int mid = (right + left) / 2;
+                if (row[mid] < 0) {
+                    right = mid;
+                } else {
+                    left = mid + 1;  
+                }
             }
-            // 'currRowNegativeIndex' points to the last positive element,
-            // which means 'n - (currRowNegativeIndex + 1)' is the number of all negative elements.
-            count += (m - (currRowNegativeIndex + 1));
+            // 'left' points to the first negative element,
+            // which means 'n - left' is the number of all negative elements.
+            count += (n - right);
         }
         return count;
     }
