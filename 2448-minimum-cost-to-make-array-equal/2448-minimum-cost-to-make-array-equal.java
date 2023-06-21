bc1b62ce6d640523222b487567fc78e803e142f8
Rhythm Jayee <jayeerythm8@gmail.com>
2023-06-21 08:24:33 +0530

    Time: 15 ms (95.38%), Space: 55.6 MB (60.00%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.java
@@ -1,40 +1,33 @@
 class Solution {
+    // Get the cost of making every element equals base.
+    private long getCost(int[] nums, int[] cost, int base) {
+        long result = 0L;
+        for (int i = 0; i < nums.length; ++i)
+            result += 1L * Math.abs(nums[i] - base) * cost[i];
+        return result;
+    }
     public long minCost(int[] nums, int[] cost) {
-
-        int[][] data = new int[nums.length][2];
-        for (int i = 0; i < nums.length; i++)
-            data[i] = new int[]{nums[i],cost[i]};
-
-        Arrays.sort(data, Comparator.comparingInt(a -> a[0]));
-
-        int idx = 0, maxCost = Integer.MIN_VALUE;
-        long[] preSum = new long[nums.length + 1];
-        for (int i = 0; i < data.length; i++) {
-            preSum[i + 1] = preSum[i] + data[i][1];
-            if (data[i][1] > maxCost){
-                idx = i;
-                maxCost = data[i][1];
-            }
+        // Initialize the left and the right boundary of the binary search.
+        int left = 1000001, right = 0;
+        for (int num : nums) {
+            left = Math.min(left, num);
+            right = Math.max(right, num);
         }
-
-        long sum = preSum[preSum.length - 1];
-        int lastIdx = -1;
-        //find index to which other numbers should be converted 
-        //inorder to get min sum
-        //taking index of number having max cost as reference
-        while (lastIdx != idx) {
-            lastIdx = idx;
-            //if forword has less cost
-            if (preSum[idx + 1] < sum - preSum[idx + 1]) idx++;
-            //if curr pos has more cost
-            else if (preSum[idx] > sum - preSum[idx]) idx--;
+        long answer = getCost(nums, cost, nums[0]);
+        
+        // As shown in the previous picture, if F(mid) > F(mid + 1), then the minimum
+        // is to the right of mid, otherwise, the minimum is to the left of mid.
+        while (left < right) {
+            int mid = (right + left) / 2;
+            long cost1 = getCost(nums, cost, mid);
+            long cost2 = getCost(nums, cost, mid + 1);
+            answer = Math.min(cost1, cost2);
+            
+            if (cost1 > cost2) 
+                left = mid + 1;
+            else
+                right = mid;
         }
-
-        long res = 0l;
-        //Now other nos will be converted to no. at idx
-        //cost will be added for abs diff to idx no.
-        for (int i = 0; i < nums.length; i++)
-            res += 1l * Math.abs(nums[i] - data[idx][0]) * cost[i];
-        return res;
+        return answer;
     }
 }
