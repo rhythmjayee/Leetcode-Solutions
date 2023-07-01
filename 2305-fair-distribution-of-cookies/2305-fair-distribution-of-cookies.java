f45456ba6d92ff9e212e1e1ada77e8fdd526d2e7
Rhythm Jayee <jayeerythm8@gmail.com>
2023-07-01 08:52:50 +0530

    Time: 19 ms (82.81%), Space: 40.2 MB (30.73%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -1,9 +1,11 @@
 class Solution {
+    //zero_count that represents the number of children without a cookie
     public int distributeCookies(int[] cookies, int k) {
         int[] ks = new int[k];
-        return dfs(cookies, 0, ks);
+        return dfs(cookies, 0, ks, k);
     }
-    public int dfs(int[] c, int i, int[] ks) {
+    public int dfs(int[] c, int i, int[] ks, int zero_count) {
+        if(c.length - i < zero_count) return Integer.MAX_VALUE;
         if(i == c.length) {
             int max = 0;
             for(int x : ks) max = Math.max(max, x);
@@ -11,9 +13,11 @@ class Solution {
         }
         int min = Integer.MAX_VALUE;
         for(int k = 0; k<ks.length; k++) {
+            zero_count -= ks[k] == 0 ? 1 : 0;
             ks[k] += c[i];
-            min = Math.min(min, dfs(c, i + 1, ks));
+            min = Math.min(min, dfs(c, i + 1, ks, zero_count));
             ks[k] -= c[i];
+            zero_count += ks[k] == 0 ? 1 : 0;
         }
         return min;
     }
