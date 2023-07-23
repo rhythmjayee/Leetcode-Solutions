e06fc9e5aa7031a2c0b6cbad7308ad338b7cd67a
Rhythm Jayee <jayeerythm8@gmail.com>
2023-07-23 10:22:28 +0530

    Time: 1 ms (98.93%), Space: 45.8 MB (60.92%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    public static Map<Integer, List<TreeNode>> dp = new HashMap<>();
     public List<TreeNode> allPossibleFBT(int n) {
         List<TreeNode> ans = new ArrayList<>();
         if(n == 0) {
@@ -22,7 +23,7 @@ class Solution {
         } else if(n == 1) {
             ans.add(new TreeNode(0));
             return ans;
-        }
+        } else if(dp.containsKey(n)) return dp.get(n);
         //loop over the number of nodes to used
         //to make subtress on left/right
         for(int i = 1; i <= n - 2; i++) {
@@ -37,6 +38,7 @@ class Solution {
                 }
             }
         }
+        dp.put(n, ans);
         return ans;
     }
 }
