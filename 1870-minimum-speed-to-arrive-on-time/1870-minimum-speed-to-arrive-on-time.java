b374564c415ae258688d49d3e3bd4d4c74710dac
Rhythm Jayee <jayeerythm8@gmail.com>
2023-07-26 08:45:28 +0530

    Time: 277 ms (10.04%), Space: 59 MB (56.40%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hours) {
+        int ans = -1;
+        int i = 1;
+        int j = (int)(1e9);
+        while(i <= j) {
+            int md = i + ((j - i) >> 1);
+            double totalTimeNeeded = TryTravelInTime(dist, md);
+            if(totalTimeNeeded <= hours) {
+                ans = md;
+                j = md - 1;
+            } else {
+                i = md + 1;
+            }
+        }
+        return ans;
+    }
+    public double TryTravelInTime(int[] dist, int speed) {
+        double time = 0.0;
+        for(int i = 0; i < dist.length; i++) {
+            double timeTookByIth = dist[i] / (double)speed;
+            double timeTillNow = time + timeTookByIth;
+            // System.out.println(timeTillNow+" "+speed );
+            if(dist.length - 1 == i) {
+                return timeTillNow;
+            }
+            time += Math.ceil(timeTookByIth);
+            
+        }
+        return 0.0;
+    }
+}
