5473eda8785db048441332bf821382b56a31121e
Rhythm Jayee <jayeerythm8@gmail.com>
2023-08-03 08:14:26 +0530

    Time: 5 ms (50.30%), Space: 41.5 MB (23.18%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,40 +1,17 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        if(digits.length() == 0) return new ArrayList<>();
-        
-        Map<Integer,char[]> map = new HashMap<>();
-        map.put(2,new char[]{'a','b','c'});
-        map.put(3,new char[]{'d','e','f'});
-        map.put(4,new char[]{'g','h','i'});
-        map.put(5,new char[]{'j','k','l'});
-        map.put(6,new char[]{'m','n','o'});
-        map.put(7,new char[]{'p','q','r','s'});
-        map.put(8,new char[]{'t','u','v'});
-        map.put(9,new char[]{'w','x','y','z'});
-        
-        List<String> res = get(digits, 0, map);
-        return res;
-    }
-    private List<String> get(String digit, int i, Map<Integer,char[]> map){
-        if(i == digit.length()){
-            List<String> res = new ArrayList<>();
-            res.add("");
-            return res;
-        }
-        
-        int d = digit.charAt(i) - '0';
-        List<String> newRes = new ArrayList<>();
-        
-        List<String> restDigit = get(digit, i+1, map);
-            
-        for(char c : map.get(d)){
-            for(String st : restDigit){
-                StringBuilder sb = new StringBuilder(c+"");
-                sb.append(st);
-                newRes.add(sb.toString());
-            }
-        }
-        
-        return newRes;
-    }
+		LinkedList<String> ans = new LinkedList<String>();
+		if(digits.isEmpty()) return ans;
+		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+		ans.add("");
+        //continue the loop till first string in ans is not same length as digits
+		while(ans.getFirst().length() != digits.length()){
+			String remove = ans.removeFirst();
+			String map = mapping[digits.charAt(remove.length())-'0'];
+			for(char c: map.toCharArray()){
+				ans.addLast(remove+c);
+			}
+		}
+		return ans;
+	}
 }
