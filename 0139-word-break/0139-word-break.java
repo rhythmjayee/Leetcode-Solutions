17466787ad2a1729bb7293635e0294f0fbb758d1
Rhythm Jayee <jayeerythm8@gmail.com>
2023-08-04 08:23:41 +0530

    Time: 5 ms (68.27%), Space: 43.7 MB (37.57%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,24 @@
+class Solution {
+    private Boolean dp[];
+    private Set<String> set;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        dp = new Boolean[n];
+        set = new HashSet<>();
+        for(String st : wordDict) set.add(st);
+        return dfs(0, s);
+    }
+    public boolean dfs(int i, String s) {
+        if(i == s.length()) return true;
+        else if(dp[i] != null) return dp[i];
+        
+        boolean canBreak = false;
+        for(int j = i; j < s.length(); j++) {
+            String ns = s.substring(i, j + 1);
+            if(set.contains(ns)) {
+                canBreak |= dfs(j + 1, s);
+            }
+        }
+        return dp[i] = canBreak;
+    }
+}
