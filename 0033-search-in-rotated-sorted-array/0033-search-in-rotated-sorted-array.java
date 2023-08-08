548c3c290c59a03f99bccc2c21c0338aebd522c9
Rhythm Jayee <jayeerythm8@gmail.com>
2023-08-08 09:02:57 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (75.49%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -7,6 +7,15 @@ class Solution {
         while(i <= j) {
             int md = i + ((j - i) >> 1);
             if(nums[md] == target) return md;
+            /*
+            i  md   j
+            1. i <= md -> [i, md] sorted
+                1.1 [i t md]
+                1.2 [i md t j]
+            2. i > md -> [md, j] sorted
+                1.1 [md t j]
+                1.2 [t md j]
+            */
             else if(nums[md] >= nums[i]) {
                 if(nums[md] > target && nums[i] <= target) {
                     j = md - 1;
