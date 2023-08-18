77c402df89ad148cb100636c96cb357a87403711
Rhythm Jayee <jayeerythm8@gmail.com>
2023-08-18 08:19:59 +0530

    Time: 2 ms (100.00%), Space: 44.2 MB (46.73%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,21 +1,62 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-        int[] inDegree = new int[n];
-        int[][] conn = new int[n][n];
-        int max = 0;
-        for(int x[] : roads) {
-            inDegree[x[0]]++;
-            inDegree[x[1]]++;
-            conn[x[0]][x[1]] = 1;
-            conn[x[1]][x[0]] = 1;
+        int result = 0;
+        int[] degrees = new int[n];
+        for(int[] road : roads) {
+            ++degrees[road[0]];
+            ++degrees[road[1]];
         }
-        for(int i = 0; i< n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                int network = inDegree[i] + inDegree[j];
-                if(conn[i][j] == 1) network--;
-                max = Math.max(max, network);
+        //first & second most value of Degree of nodes
+        int first = 0;
+        int second = 0;
+        //first & second will only hold diff values
+        for(int degree : degrees) {
+            if (degree > first) {
+                second = first;
+                first = degree;
+            } else if (degree == first) {
+                continue;
+            } else if (degree > second) {
+                second = degree;
             }
         }
-        return max;
+        
+        //count no.of nodes having same first & second values of Degree
+        int firstCount = 0;
+        int secondCount = 0;
+        for(int degree : degrees) {
+            if (degree == first) {
+                ++firstCount;
+            } else if (degree == second) {
+                ++secondCount;
+            }
+        }
+        //if one node is having first value degree 
+        if (firstCount == 1) {
+            //count roads which is having same first & second value degree nodes
+            //for removing the common edge from network
+            int count = 0;
+            for(int[] road : roads) {
+                if (degrees[road[0]] == first && degrees[road[1]] == second) {
+                    ++count;
+                } else if (degrees[road[1]] == first && degrees[road[0]] == second) {
+                    ++count;
+                }
+            }
+            //secondCount == count -> 
+            //all second value degree nodes all connected with first degree nodes
+            //There is no 2 nodes are not connected and have first & second degress
+            return first + second - (secondCount == count ? 1 : 0);
+        } else {//more than 2 first degree nodes
+            int count = 0;
+            for(int[] road : roads) {
+                if (degrees[road[0]] == first && degrees[road[1]] == first) {
+                    ++count;
+                }
+            }
+            //firstCount * (firstCount - 1) / 2 > count -> 
+            //all first degree nodes not connected with each other
+            return first + first - (firstCount * (firstCount - 1) / 2 > count ? 0 : 1);
+        }
     }
 }
