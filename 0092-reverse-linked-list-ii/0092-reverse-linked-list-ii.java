d0b4d0572da1dcd15777067661ebbf3b3a3b6352
Rhythm Jayee <jayeerythm8@gmail.com>
2023-09-07 10:01:13 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (50.29%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -11,33 +11,36 @@
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         if(head == null || head.next == null || left == right) return head;
-        
-        ListNode d = new ListNode(0);
-        d.next = head;
-        head = d;
-        
-        ListNode prev = d;
-        ListNode next = null;
-        int count = 0;
-        ListNode rv = null;
-        
-        while(head != null && count <= right) {
-            ListNode nx = head.next;
-            if(count == left - 1) {
+        //LL_front -> LL_rev -> LL_back
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        int idx = 1;
+        while(head != null) {
+            ListNode tmp = head;
+            if(idx == left) {
+                while(tmp != null && idx != right) {
+                    tmp = tmp.next;
+                    idx++;
+                }
+                //pointer to LL_back
+                ListNode nxt = tmp.next;
+                tmp.next = null;
+                ListNode rv = reverseList(head);//LL_rev
+                //connect LL_front -> LL_rev
+                prev.next = rv;
+                //connect LL_rev -> LL_back
+                head.next = nxt;
+                break;
+            } else {
+                //prev will be iterating over the LL_front
+                prev.next = head;
                 prev = head;
-                rv = head.next;
+                head = head.next;
+                idx++;
             }
-            if(count == right) {
-                head.next = null;
-            }
-            head = nx;
-            count++;
         }
-        ListNode newRvH = reverseList(rv);
-        prev.next = newRvH;
-        rv.next = head;
         
-        return d.next;
+        return dummy.next;
     }
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) return head;
