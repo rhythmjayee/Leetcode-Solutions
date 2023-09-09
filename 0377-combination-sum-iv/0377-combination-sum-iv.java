50e4a95c6ab98e7dd90426ce354bb40b7302ce1f
Rhythm Jayee <jayeerythm8@gmail.com>
2023-09-09 09:57:35 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (61.50%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,21 +1,21 @@
 class Solution {
-    Integer[][] dp;
+    Integer[] dp;
     public int combinationSum4(int[] nums, int target) {
         Arrays.sort(nums);
-        dp = new Integer[nums.length][target + 1];
+        dp = new Integer[target + 1];
         return dfs(nums, 0, target);
     }
     public int dfs(int[] nums, int i, int t) {
         if(t == 0) return 1;
         else if (i == nums.length) return 0;
-        else if(dp[i][t] != null) return dp[i][t];
+        else if(dp[t] != null) return dp[t];
         int count = 0;
         for(int k = 0; k < nums.length; k++) {
             if(t - nums[k] >= 0) {
                 count += dfs(nums, k, t - nums[k]);
             } else break;
         }
-        return dp[i][t] = count;
+        return dp[t] = count;
     }
 }
 /*
