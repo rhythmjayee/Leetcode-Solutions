c9513ef7e2e6240d4d5b2f6edb390e005c6acffc
Rhythm Jayee <jayeerythm8@gmail.com>
2023-09-09 10:03:44 +0530

    Time: 1 ms (72.35%), Space: 39.8 MB (61.50%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,6 +1,6 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
         int[] dp = new int[target + 1];
         dp[0] = 1;
         
@@ -8,7 +8,8 @@ class Solution {
             for(int j = 0; j < nums.length; j++) {
                 if(i - nums[j] >= 0) {
                     dp[i] += dp[i - nums[j]];
-                } else break;
+                } 
+                // else break;
             }
         }
         return dp[target];
