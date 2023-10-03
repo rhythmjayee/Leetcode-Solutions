aa74ebc5660f1c274a5740e034b48ab8bfda2a8f
Rhythm Jayee <jayeerythm8@gmail.com>
2023-10-03 08:49:02 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (73.19%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -2,12 +2,14 @@ class Solution {
     public int numIdenticalPairs(int[] nums) {
         int[] f = new int[101];
         int sum = 0;
-        for(int x : nums) f[x]++;
-        for(int i = 1; i<=100; i++) {
-            if(f[i] > 1) {
-                sum += ((f[i] * (f[i] - 1)) >> 1);
-            }
+        for(int x : nums) {
+            sum += f[x]++;
         }
+        // for(int i = 1; i<=100; i++) {
+        //     if(f[i] > 1) {
+        //         sum += ((f[i] * (f[i] - 1)) >> 1);
+        //     }
+        // }
         return sum;
     }
 }
