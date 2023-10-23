ba55eb8f0076f32f8e36f2878925d5654da60583
Rhythm Jayee <jayeerythm8@gmail.com>
2023-10-23 14:41:28 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (93.34%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
@@ -2,15 +2,11 @@ class Solution {
     public int maxChunksToSorted(int[] arr) {
         Deque<Integer> sk = new LinkedList<>();
         for(int x : arr) {
-            if(!sk.isEmpty() && sk.getLast() > x) {
-                int max = 0;
-                while(!sk.isEmpty() && sk.getLast() > x) {
-                    max = Math.max(max, sk.removeLast());
-                }
-                sk.addLast(max);
-            } else {
-                sk.addLast(x);
+            int max = x;
+            while(!sk.isEmpty() && sk.getLast() > x) {
+                max = Math.max(max, sk.removeLast());
             }
+            sk.addLast(max);
         }
         return sk.size();
     }
