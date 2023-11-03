a8b003825de128bc402a7652451dc8b04ad7b6ad
Rhythm Jayee <jayeerythm8@gmail.com>
2023-11-03 06:18:59 +0530

    Time: 8 ms (96.24%), Space: 44.2 MB (73.66%) - LeetHub

--- 1124-longest-well-performing-interval/1124-longest-well-performing-interval.java
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.java
@@ -1,18 +1,20 @@
 class Solution {
     public int longestWPI(int[] hours) {
-        int res = 0, score = 0, n = hours.length;
-        Map<Integer, Integer> seen = new HashMap<>();
-        for (int i = 0; i < n; ++i) {
-            score += hours[i] > 8 ? 1 : -1;
-            if (score > 0) {
-                res = i + 1;//if > 0, means well-performing interval
-            } else {
-                //put first occurance of score only to make interval longest
-                seen.putIfAbsent(score, i);
-                //find score - 1, on removing this part from interval
-                //our current score in new interval will be 1 > 0
-                if (seen.containsKey(score - 1))
-                    res = Math.max(res, i - seen.get(score - 1));
+        int len = hours.length;
+        int[] preSum = new int[len+1];   // prefix Sum
+        for (int i = 1; i <= len; i++) {
+            preSum[i] = preSum[i-1] + (hours[i-1] > 8 ? 1 : -1);
+        }
+        Deque<Integer> stack = new LinkedList<>();   // Deque (8ms) is much faster than Stack (18ms)
+        for (int i = 0; i <= len; i++) {
+            if (stack.isEmpty() || preSum[stack.getLast()] > preSum[i]) {
+                stack.addLast(i);
+            }
+        }
+        int res = 0;
+        for (int j = len; j >= 0; j--) {  // start from end
+            while (!stack.isEmpty() && preSum[stack.getLast()] < preSum[j]) {
+                res = Math.max(res, j-stack.removeLast());
             }
         }
         return res;
