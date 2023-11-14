65fff04f714aa4dcc0207751183350ec85b5d456
Rhythm Jayee <jayeerythm8@gmail.com>
2023-11-14 09:45:19 +0530

    Time: 181 ms (62.25%), Space: 44.4 MB (53.19%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        //get first & last index of character
+        //try to make Palindrome with fixing each char on first & last position
+        //and then fix any other char bw them
+        int[] first = new int[26];
+        int[] last = new int[26];
+        Arrays.fill(first, -1);
+        
+        for (int i = 0; i < s.length(); i++) {
+            int curr = s.charAt(i) - 'a';
+            if (first[curr] == - 1) {
+                first[curr] = i;
+            }
+            
+            last[curr] = i;
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (first[i] == -1) {
+                continue;
+            }
+            //get all unique char seen
+            Set<Character> between = new HashSet();
+            for (int j = first[i] + 1; j < last[i]; j++) {
+                between.add(s.charAt(j));
+            }
+            
+            ans += between.size();
+        }
+        
+        return ans;
+    }
+}
