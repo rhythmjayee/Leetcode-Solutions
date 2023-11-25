2a0d46b5a32e695b57ffc551cd84d0027f7833d9
Rhythm Jayee <jayeerythm8@gmail.com>
2023-11-25 08:20:11 +0530

    Time: 5 ms (25.64%), Space: 59.7 MB (12.82%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -2,26 +2,31 @@ class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
         int n = nums.length;
         int lSum = 0;
-        int[][] numsi = new int[n][2];
-        int[] rightSum = new int[n];
         
+        int totalSum = 0;
+        for(int x : nums) totalSum += x;
+        
+        /**
+        If array is not sorted, below steps is required as
+        we want element on left side smaller so that we have +ve diff
+        and elements on right are larger so more -ve diff on right
+        
+        int[][] numsi = new int[n][2];
         for(int i = n - 1; i >= 0; i--) {
             numsi[i][0] = nums[i];
             numsi[i][1] = i;
         }
         Arrays.sort(numsi, (a, b) -> a[0] - b[0]);
-        rightSum[n - 1] = numsi[n - 1][0];
-        for(int i = n - 2; i >= 0; i--) {
-            rightSum[i] = rightSum[i + 1] + nums[i];
-        }
+        ***/
+
         int[] ans = new int[n];
         for(int i = 0; i < n; i++) {
-            int restSum = rightSum[i] - numsi[i][0];
-            long selfSumLeft = i*numsi[i][0];
-            long selfSumRight = (n-i-1)*numsi[i][0];
+            int restSum = totalSum - nums[i] - lSum;
+            long selfSumLeft = i*nums[i];
+            long selfSumRight = (n-i-1)*nums[i];
             long diff = Math.abs(selfSumLeft - lSum) +  Math.abs(selfSumRight - restSum);
-            ans[numsi[i][1]] = (int)diff;
-            lSum += numsi[i][0];
+            ans[i] = (int)diff;
+            lSum += nums[i];
         }
         return ans;
     }
