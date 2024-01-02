3af04ffe2b7ab0e7192033a6cdda843168591d32
Rhythm Jayee <jayeerythm8@gmail.com>
2024-01-02 11:58:32 +0530

    Time: 4 ms (55.08%), Space: 45.3 MB (9.75%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,29 +1,18 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int n = nums.length;
-        int max = 0;
-        for(int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            max = Math.max(max, map.get(nums[i]));
-        }
-        
+        int freq[] = new int[nums.length + 1];
+
         List<List<Integer>> ans = new ArrayList<>();
-        int op = 0;
-        while(max > 0) {
-            List<Integer> ls = new ArrayList<>();
-            Set<Integer> set = map.keySet();
-            for(int k : set) {
-                int val = map.get(k);
-                if(val - op <= 0) {
-                    continue;
-                }
-                ls.add(k);
+        for (int c : nums) {
+            //add new list for number c, as no. of lists <= freq of c
+            if (++freq[c] > ans.size()) {
+                ans.add(new ArrayList<>());
             }
-            max--;
-            op++;
-            ans.add(ls);
+
+            // Store the integer in the list corresponding to its current frequency.
+            ans.get(freq[c] - 1).add(c);
         }
+
         return ans;
     }
 }
