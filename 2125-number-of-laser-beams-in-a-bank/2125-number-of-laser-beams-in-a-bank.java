1fa96a0b1e2574189b68459fb4d22bb2f8215ff7
Rhythm Jayee <jayeerythm8@gmail.com>
2024-01-03 11:35:31 +0530

    Time: 22 ms (16.47%), Space: 44.7 MB (34.52%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,11 +1,13 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
         int n = bank.length;
+        int m = bank[0].length();
         int beans = 0;
         int prev = 0;
         for(int i = 0; i < n; i++) {
             int securityDevices = 0;
-            for(char c : bank[i].toCharArray()) {
+            for(int j = 0; j < m; j++) {
+                char c = bank[i].charAt(j);
                 if(c == '1') securityDevices++;
             }
             if(prev > 0)
