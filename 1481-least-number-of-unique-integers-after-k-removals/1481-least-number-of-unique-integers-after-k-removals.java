4adcac5d80115a21729f8ea8b73402419d4252f4
Rhythm Jayee <jayeerythm8@gmail.com>
2024-02-16 08:36:03 +0530

    Time: 37 ms (92.43%), Space: 56.8 MB (80.00%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -1,20 +1,45 @@
 class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        // Map to track the frequencies of elements
         Map<Integer, Integer> map = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
-            return map.get(a) - map.get(b);
-        });
-        
-        for(int x : arr) map.put(x, map.getOrDefault(x, 0) + 1);
-        for(int x : map.keySet()) pq.add(x);
-        System.out.println(pq);
-        while(k > 0 && !pq.isEmpty()) {
-            int x = pq.remove();
-            int freq = map.get(x);
-            if(freq > k) return pq.size() + 1;
-            
-            k -= freq;
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        return pq.size();
+
+        int n = arr.length;
+        int[] countOfFrequencies = new int[n + 1];
+
+        for (int count : map.values()) {
+            countOfFrequencies[count]++;
+        }
+
+        int remainingUniqueElements = map.size();
+
+        for (int i = 1; i <= n; i++) {
+            // For each possible frequency i, we'd like to remove as
+            // many elements with that frequency as possible.
+            // k / i represents the number of maximum possible elements
+            // we could remove with k elements left to remove.
+            // countOfFrequencies[i] represents the actual number of elements
+            // with frequency i.
+            int numElementsToRemove = Math.min(k / i, countOfFrequencies[i]);
+
+            // Removing maximum possible elements
+            k -= (i * numElementsToRemove);
+
+            remainingUniqueElements -= numElementsToRemove;
+
+            // If the number of elements that can be removed is less
+            // than the current frequency, we won't be able to remove
+            // any more elements with a higher frequency so we can return
+            // the remaining number of unique elements
+            if (k < i) {
+                return remainingUniqueElements;
+            }
+        }
+
+        // We have traversed all possible frequencies i.e.,
+        // removed all elements. Returning 0 in this case.
+        return 0;
     }
 }
