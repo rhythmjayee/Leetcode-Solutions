d99641b7e12b7767613a2c09f85642430b09cd23
Rhythm Jayee <jayeerythm8@gmail.com>
2024-04-04 09:25:54 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (40.14%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxDepth(String s) {
+        int max = 0;
+        int curr = 0;
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
+                curr++;
+                max = Math.max(max, curr);
+            } else if (c == ')') {
+                curr--;
+            }
+        }
+        return max;
+    }
+}
